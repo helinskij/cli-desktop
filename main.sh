8f15958453adc8874381785ca6f002a05e18665a
TuringProblem <andrew.deca.wellington@gmail.com>
2024-12-13 03:34:14 +0000

    feat: started working on the structure of this program

--- main.sh
+++ main.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
+function devTag() {
+  jLinksky="JAKUB"
+  aWinsky="ANDREW"
+  message="Copyright © Developers: ${jLinksky} && ${aWinsky}"
+  echo ${message}
+}
 
 echo hello world
 function interface() {
   type=$1
 
+  case "$type" in
+    0) 
+      printf '=%.0s' {1..100}
+      printf "\n\n"
+      echo '- [1] Previte'
+      printf "\n\n"
 
+  esac
 }
-
-
+echo "Loading..."
 sleep 5
+
+interface 0
+
 echo "helloworld"
+devTag
